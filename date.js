26bf54c24556adc1d902d767fec5b8033058d761
infiniciencia <155041171+infiniciencia@users.noreply.github.com>
2023-12-31 13:44:13 -0300

    Delete date.js

--- date.js
+++
@@ -1,7 +0,0 @@
-const currentDateElement = document.getElementById("currentDate");
-
-const currentDate = new Date();
-
-const formattedDate = `${currentDate.getDate()}/${currentDate.getMonth() + 1}/${currentDate.getFullYear()}`;
-
-currentDateElement.textContent = formattedDate;
