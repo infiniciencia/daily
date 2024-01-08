e79b46b64ccf6824e4118307670527e6f0ea20b6
infiniciencia <155041171+infiniciencia@users.noreply.github.com>
2024-01-08 06:28:35 -0300

    Update text.js

--- text.js
+++ text.js
@@ -10,7 +10,8 @@ function exibirMensagemDoDia() {
 	"2024-01-04": ["Pensando em você..."],
 	"2024-01-05": ["Em um emaranhamento de destinos,", "nossos corações dançam como partículas entrelaçadas.", "Na mecânica quântica do amor,", "somos como átomos conectados por forças invisíveis.", "Nossos momentos juntos são como superposição de estados,", "onde a incerteza se dissolve na certeza de que,", "mesmo em diferentes espaços, nossas ondas se encontram.", "~", "Em que nível subatômico nossos corações dançam", "em um entrelaçamento de amor?"],
 	"2024-01-06": ["Eu posso não ter ido para onde eu pretendia ir,", "mas eu acho que acabei terminando onde eu pretendia estar.", "~", "- Douglas Adams"],
-	"2024-01-07": ["Você é a constante do meu coração,", "a variável que completa a equação do meu amor."]
+	"2024-01-07": ["Você é a constante do meu coração,", "a variável que completa a equação do meu amor."],
+	"2024-01-08": ["Scientific progress is measured in units of courage, not intelligence.", "~", "- Paul Dirac"],
     };
 
     const dataAtual = new Date();
